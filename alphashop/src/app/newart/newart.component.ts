51af2ca3282f3446a0688076dc1e58c7abdc2eee
Paulpoli1 <pauluni10@hotmail.com>
2020-07-05 12:28:52 +0200

    107.Creazione della form inserimento dati articoli parte 1

--- alphashop/src/app/newart/newart.component.ts
+++ alphashop/src/app/newart/newart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Articoli } from '../articoli/articoli.component';
+import { ArticoliDataService } from '../services/articoli-data.service';
 
 @Component({
   selector: 'app-newart',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private articoliService: ArticoliDataService) { }
+
+  CodArt : String = "";
+  articolo: Articoli;
 
   ngOnInit(): void {
+    this.CodArt = this.route.snapshot.params['codart'];
+
+    this.articoliService.getArticoliByCordArt(this.CodArt).subscribe(
+      response => {
+        this.articolo = response;
+        console.log(this.articolo);
+      },
+      error => {
+        console.log(error.error.messaggio);
+        
+      }
+    );
+
   }
 
 }
