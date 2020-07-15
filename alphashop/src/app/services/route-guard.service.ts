9c7d877a8501d1d5a41f4de266d6349e77468f33
Paulpoli1 <pauluni10@hotmail.com>
2020-07-15 20:11:01 +0200

    139 implementazione la sicurezza basata sul ruolo parte 2

--- alphashop/src/app/services/route-guard.service.ts
+++ alphashop/src/app/services/route-guard.service.ts
@@ -1,21 +1,48 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { AuthappService } from './authapp.service';
+import { AuthJWTService } from './authJWT.service';
+import { JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RouteGuardService implements CanActivate {
+  token: string = '';
+  ruoli: string[] = new Array();
+  items: any;
+
+  constructor(private BasicAuth: AuthJWTService, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+    this.token = this.BasicAuth.getAuthToken();
+    const helper = new JwtHelperService();
+    const decodedToken = helper.decodeToken(this.token);
+
+    this.items = decodedToken['role'];
+
+    if(!Array.isArray(this.items))
+    {
+      this.ruoli.push(this.items)
+    }
+    else{
+      this.ruoli = this.items;
+    }
+
     if(!this.BasicAuth.isLogged()){
       this.router.navigate(['login']);
       return false;
     }
     else{
-      return true;
+      if(route.data.roles == null || route.data.roles.length ===0){
+        return true;
+      }
+      else if(this.ruoli.some(r => route.data.roles.includes(r))){
+        return true;
+      }
+      else {
+        return false;
+        //da gestire la pagina forbidden
+      }
     }
   }
-
-  constructor(private BasicAuth: AuthappService, private router: Router) { }
 }
